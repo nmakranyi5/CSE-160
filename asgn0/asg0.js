0a762f8e610ff009547c6090a37bc1a7fb385dd1
Nikolas Makranyi <146390954+nmakranyi5@users.noreply.github.com>
2026-04-01 11:59:38 -0700

    finished part 5, completed add, sub, mult , and div functions, and drawing these changes on the canvas

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -42,3 +42,45 @@ function handleDrawEvent() {
   const v2 = new Vector3([parseFloat(x2), parseFloat(y2), 0]);
   drawVector(v2, "blue");
 }
+
+function handleDrawOperationEvent() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing canvas
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  let x1 = document.getElementById("x1").value;
+  let y1 = document.getElementById("y1").value;
+  const v1 = new Vector3([parseFloat(x1), parseFloat(y1), 0]);
+  drawVector(v1, "red");
+
+  let x2 = document.getElementById("x2").value;
+  let y2 = document.getElementById("y2").value;
+  const v2 = new Vector3([parseFloat(x2), parseFloat(y2), 0]);
+  drawVector(v2, "blue");
+
+  let operation = document.getElementById("operation").value;
+  let scalar = parseFloat(document.getElementById("scalar").value);
+  const v3 = new Vector3([v1.elements[0], v1.elements[1], v1.elements[2]]);
+  const v4 = new Vector3([v2.elements[0], v2.elements[1], v2.elements[2]]);
+  if (operation === "add") {
+    v3.add(v2);
+    drawVector(v3, "green");
+  }
+  else if (operation === "sub") {
+    v3.sub(v2);
+    drawVector(v3, "green");
+  }
+  else if (operation === "mul") {
+    v3.mul(scalar);
+    v4.mul(scalar);
+    drawVector(v3, "green");
+    drawVector(v4, "green");
+  }
+  else if (operation === "div")
+  {
+    v3.div(scalar);
+    v4.div(scalar);
+    drawVector(v3, "green");
+    drawVector(v4, "green");
+  }
+}
