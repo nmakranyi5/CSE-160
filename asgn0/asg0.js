4283af8d0d7e08a0b060bba7c86abfe3c184533b
Nikolas Makranyi <146390954+nmakranyi5@users.noreply.github.com>
2026-04-01 00:22:40 -0700

    finished parts 1 and 2

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -1,16 +1,29 @@
+let canvas;
+let ctx;
+
 // DrawTriangle.js (c) 2012 matsuda
-function main() {  
+function main() {
   // Retrieve <canvas> element
-  var canvas = document.getElementById('example');  
-  if (!canvas) { 
-    console.log('Failed to retrieve the <canvas> element');
-    return false; 
-  } 
-
+  canvas = document.getElementById('example');
   // Get the rendering context for 2DCG
-  var ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
+  if (!canvas) {
+    console.log('Failed to retrieve the <canvas> element');
+    return false;
+  }
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  // Draw a black background
+  ctx.fillStyle = "black"; // Set color to black
+  ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill a rectangle with the color
+
+  const v1 = new Vector3([2.25, 2.25, 0]);
+  drawVector(v1, 'red');
+}
+
+function drawVector(v, color) {
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2, canvas.height / 2);
+  ctx.lineTo(canvas.width / 2 + v.elements[0] * 20, canvas.height / 2 - v.elements[1] * 20);
+  ctx.strokeStyle = color;
+  ctx.stroke();
 }
